267c60cfe8c18ab10d457851fb6a03399e2a8aae
jared214 <77410008+jared214@users.noreply.github.com>
2025-11-28 07:12:35 -0500

    recipeBdetail.js blind edit
    did it work?

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -6,6 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
@@ -14,6 +15,7 @@ const firebaseConfig = {
   messagingSenderId: "",
   appId: ""
 };
+
 
 
 // Initialize Firebase
