784a9ce171be78dc0af8db1b460dee2539735f8a
WesleyHui <72075816+Wesley436@users.noreply.github.com>
2025-11-28 09:31:05 -0500

    Revert "recipeBdetail.js blind edit"
    This reverts commit 267c60cfe8c18ab10d457851fb6a03399e2a8aae.

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -6,7 +6,6 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
@@ -15,7 +14,6 @@ const firebaseConfig = {
   messagingSenderId: "",
   appId: ""
 };
-
 
 
 // Initialize Firebase
